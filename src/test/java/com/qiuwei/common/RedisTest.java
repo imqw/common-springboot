e76ca6148792fc8e3ca0a041598c35bc75fc5ac8
qiuwei <Wcbc0310@126.com>
2019-06-13 20:11:44 +0800

    添加redis配置Lettuce版本

---
+++ src/test/java/com/qiuwei/common/RedisTest.java
@@ -0,0 +1,42 @@
+package com.qiuwei.common;
+
+import com.qiuwei.common.demo.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @Author: qiuwei@19pay.com.cn
+ * @Version 1.0.0
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class RedisTest {
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+
+    @Test
+    public void redisTest() {
+        // redis存储数据
+        String key = "name";
+        redisTemplate.opsForValue().set(key, "yukong");
+        // 获取数据
+        String value = (String) redisTemplate.opsForValue().get(key);
+        System.out.println("获取缓存中key为" + key + "的值为：" + value);
+
+        User user = new User("qw",12);
+
+        String userKey = "yukong";
+        redisTemplate.opsForValue().set(userKey, user);
+        User newUser = (User) redisTemplate.opsForValue().get(userKey);
+        System.out.println("获取缓存中key为" + userKey + "的值为：" + newUser);
+
+    }
+
+
+
+}
